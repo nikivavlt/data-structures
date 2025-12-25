3789e4e543ca2ad4d601407422fc378d596f9066
nikivavlt <nikivavlt@gmail.com>
2025-12-25 08:40:35 +0200

    Add LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import datastructures.LinkedList;
 import datastructures.Queue;
 import datastructures.Stack;
 
@@ -5,36 +6,44 @@ import datastructures.Stack;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Queue<String> q = new Queue<String>();
+//        Queue<String> q = new Queue<String>();
+//
+//        System.out.println("Empty? " + q.isEmpty());  // true
+//
+//        q.add("First");
+//        q.add("Second");
+//        q.add("Third");
+//
+//        System.out.println("Size: " + q.size());      // 3
+//        System.out.println("Peek: " + q.peek());      // First
+//        System.out.println("Remove: " + q.remove());  // First
+//        System.out.println("Remove: " + q.remove());  // Second
+//        System.out.println("Size: " + q.size());      // 1
+//        System.out.println("Remove: " + q.remove());  // Third
+//        System.out.println("Empty? " + q.isEmpty());  // true
+//
+//        Stack<String> s = new Stack<String>();
+//
+//        System.out.println("Empty? " + s.isEmpty());  // true
+//
+//        s.push("First");
+//        s.push("Second");
+//        s.push("Third");
+//
+//        System.out.println("Size: " + s.size());      // 3
+//        System.out.println("Peek: " + s.peek());      // Third
+//        System.out.println("Pop: " + s.pop());        // Third
+//        System.out.println("Pop: " + s.pop());        // Second
+//        System.out.println("Size: " + s.size());      // 1
+//        System.out.println("Pop: " + s.pop());        // First
+//        System.out.println("Empty? " + s.isEmpty());  // true
 
-        System.out.println("Empty? " + q.isEmpty());  // true
-
-        q.add("First");
-        q.add("Second");
-        q.add("Third");
-
-        System.out.println("Size: " + q.size());      // 3
-        System.out.println("Peek: " + q.peek());      // First
-        System.out.println("Remove: " + q.remove());  // First
-        System.out.println("Remove: " + q.remove());  // Second
-        System.out.println("Size: " + q.size());      // 1
-        System.out.println("Remove: " + q.remove());  // Third
-        System.out.println("Empty? " + q.isEmpty());  // true
-
-        Stack<String> s = new Stack<String>();
-
-        System.out.println("Empty? " + s.isEmpty());  // true
-
-        s.push("First");
-        s.push("Second");
-        s.push("Third");
-
-        System.out.println("Size: " + s.size());      // 3
-        System.out.println("Peek: " + s.peek());      // Third
-        System.out.println("Pop: " + s.pop());        // Third
-        System.out.println("Pop: " + s.pop());        // Second
-        System.out.println("Size: " + s.size());      // 1
-        System.out.println("Pop: " + s.pop());        // First
-        System.out.println("Empty? " + s.isEmpty());  // true
+        LinkedList<String> list = new LinkedList<>();
+        list.add("A");
+        list.add("C");
+        list.add(1, "B");
+        System.out.println(list.get(1));  // "B"
+        list.remove(1);
+        System.out.println(list.indexOf("C"));  // 1
     }
 }
---
+++ src/datastructures/LinkedList.java
@@ -0,0 +1,144 @@
+package datastructures;
+
+import java.util.Objects;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    private Node<T> getNode(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index " + index + ", Size " + size);
+
+        Node<T> currentItem = head;
+
+        for (int i = 0; i < index; i += 1) {
+            currentItem = currentItem.next;
+        }
+
+        return currentItem;
+    }
+
+    public void add(T data) {
+        Node<T> newItem = new Node<T>(data);
+
+        if (isEmpty()) {
+            head = newItem;
+        } else {
+            tail.next = newItem;
+        }
+        tail = newItem;
+        size += 1;
+    }
+
+    public void add(int index, T data) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException("Index " + index + ", Size " + size);
+
+        Node<T> newItem = new Node<T>(data);
+
+        if (index == 0) {
+            if (isEmpty()) {
+                tail = newItem;
+            }
+
+            newItem.next = head;
+            head = newItem;
+        }
+        else if (index == size) {
+            tail.next = newItem;
+            tail = newItem;
+        } else {
+            Node<T> frontNode = getNode(index - 1);
+            newItem.next = frontNode.next;
+            frontNode.next = newItem;
+        }
+
+        size += 1;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index " + index + ", Size " + size);
+
+        Node<T> selectedNode = getNode(index);
+
+        if (index == 0) {
+            head = head.next;
+
+            if (head == null) {
+                tail = null;
+            }
+        } else if (index == size - 1) {
+            Node <T> frontNode = getNode(index - 1);
+            frontNode.next = null;
+            tail = frontNode;
+        } else {
+            getNode(index - 1).next = selectedNode.next;
+        }
+
+        size -= 1;
+        return selectedNode.data;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index " + index + ", Size " + size);
+        return getNode(index).data;
+    }
+
+    public void set(int index, T data) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index " + index + ", Size " + size);
+        getNode(index).data = data;
+    }
+
+    public int indexOf(T data) {
+        Node<T> currentItem = head;
+        int index = 0;
+
+        while (currentItem != null) {
+            if (Objects.equals(currentItem.data, data)) return index;
+
+            currentItem = currentItem.next;
+            index += 1;
+        }
+        return -1;
+    }
+
+    public boolean remove(T data) {
+        Node<T> currentItem = head;
+        int index = 0;
+
+        while (currentItem != null) {
+            if (Objects.equals(currentItem.data, data)) {
+                if (index == 0) {
+                   head = head.next;
+                } else {
+                    getNode(index - 1).next = currentItem.next;
+                }
+
+                if (currentItem.next == null) {
+                    tail = (index == 0) ? null : getNode(index - 1);
+                }
+
+                size -= 1;
+                return true;
+            }
+
+            currentItem = currentItem.next;
+            index += 1;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+}
